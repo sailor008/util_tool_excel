65bf38f03dbbd90b0f9912a8edef686bdfb9c41a
sailor-PC <sailor_work@163.com>
2020-01-01 23:24:22 +0800

    修改获取表头标题的逻辑

--- core/ExcelUtil.py
+++ core/ExcelUtil.py
@@ -10,25 +10,27 @@ def GetTableItemTitles(excelFilePath):
 	# sheetNames = data.sheet_names()    #返回book中所有工作表的名字
 	# for tempStr in sheetNames:
 	# 	table = data.sheet_by_name(tempStr)
-	# 	nrows = table.nrows  #获取该sheet中的有效行数
-	# 	ncols = table.ncols  #获取列表的有效列数
-	# 	print("tableName = {0}, rows = {1}, cols = {2}".format(tempStr, nrows, ncols))
 	table = excel.sheet_by_index(0)
 	row_len = table.nrows  #获取该sheet中的有效行数
 	col_len = table.ncols  #获取列表的有效列数
 	print("rows = {0}, cols = {1}".format(row_len, col_len))
-	# for item in table.row_slice(8):
-	# 	print("_____"+str(item))
 	if row_len <= 0 or col_len <= 0 :
 		return None
-	startRowIdx = 0
-	startColIdx = 0
+	#todo 从表头开始遍历，找到第一个 背景填充的格子，认定其为表格标题 开始
+	targetRow = None
+	targetCol = None
 	for rowIdx in range(row_len):
 		for colIdx in range(col_len):
-			if rowIdx >= 5:
-				break
 			xfx = table.cell_xf_index(rowIdx, colIdx)
 			xf = excel.xf_list[xfx]
-			bgx = xf.background
-			print(bgx.__dict__)
+			isCellBgFill = xf.background.fill_pattern
+			if isCellBgFill == 1:
+				targetRow = rowIdx
+				targetCol = colIdx
+				break
+	if targetRow == None or targetCol == None:
+		return None
+	print("表格标题开始的格子：(%s, %s)"%(targetRow, targetCol))
+	for cell in table.row_slice(targetRow):
+		print("_____"+str(cell))
 
